e74a78ced572fc55a1c77ea12a7e2d720fcf321d
Alex <oleksiymosiychuk108@gmail.com>
2026-01-05 21:55:45 +0300

    feat: add views and urls for Moto views

--- vehicle/urls.py
+++ vehicle/urls.py
@@ -1,14 +1,20 @@
+from django.urls import path
+
 from vehicle.apps import VehicleConfig
 from rest_framework.routers import DefaultRouter
 
-from vehicle.views import CarViewSet
+from vehicle.views import (CarViewSet, MotoListAPIView, MotoCreateAPIView, MotoUpdateAPIView, MotoDestroyAPIView,
+                           MotoRetrieveAPIView)
 
 app_name = VehicleConfig.name
-
 
 router = DefaultRouter()
 router.register(r'cars', CarViewSet, basename='cars')
 
 urlpatterns = [
-
+    path("moto/", MotoListAPIView.as_view(), name="moto_list"),
+    path("moto/<int:pk>/", MotoRetrieveAPIView.as_view(), name="moto_details"),
+    path("moto/create/", MotoCreateAPIView.as_view(), name="moto_create"),
+    path("moto/<int:pk>/update/", MotoUpdateAPIView.as_view(), name="moto_update"),
+    path("moto/<int:pk>/delete/", MotoDestroyAPIView.as_view(), name="moto_delete"),
 ] + router.urls
--- vehicle/views.py
+++ vehicle/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.generics import ListAPIView, UpdateAPIView, CreateAPIView, RetrieveAPIView, DestroyAPIView
 
 from .models import Car, Moto
 from .serializers import CarSerializer, MotoSerializer
@@ -7,3 +8,27 @@ from .serializers import CarSerializer, MotoSerializer
 class CarViewSet(viewsets.ModelViewSet):
     queryset = Car.objects.all()
     serializer_class = CarSerializer
+
+
+class MotoListAPIView(ListAPIView):
+    queryset = Moto.objects.all()
+    serializer_class = MotoSerializer
+
+
+class MotoUpdateAPIView(UpdateAPIView):
+    queryset = Moto.objects.all()
+    serializer_class = MotoSerializer
+
+
+class MotoCreateAPIView(CreateAPIView):
+    serializer_class = MotoSerializer
+
+
+class MotoRetrieveAPIView(RetrieveAPIView):
+    queryset = Moto.objects.all()
+    serializer_class = MotoSerializer
+
+
+class MotoDestroyAPIView(DestroyAPIView):
+    queryset = Moto.objects.all()
+    serializer_class = MotoSerializer
