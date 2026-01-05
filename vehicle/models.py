27288c102ddfe25dadd836435d98c2fd34aef224
Alex <oleksiymosiychuk108@gmail.com>
2026-01-05 21:22:24 +0300

    feat: add models of Car and Moto and makemigrations

--- vehicle/models.py
+++ vehicle/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Car(models.Model):
+    title = models.CharField(max_length=150, verbose_name="Название")
+    description = models.TextField(verbose_name="Описание")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "авто"
+        verbose_name_plural = "авто"
+        ordering = ['title']
+
+
+class Moto(models.Model):
+    title = models.CharField(max_length=150, verbose_name="Название")
+    description = models.TextField(verbose_name="Описание")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "мотоцикл"
+        verbose_name_plural = "мотоциклы"
+        ordering = ['title']
