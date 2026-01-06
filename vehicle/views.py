1d155e2bcc0501042639cb7d7fa0f825c23c5d8b
Alex <oleksiymosiychuk108@gmail.com>
2026-01-06 22:30:56 +0300

    feat: add filters for mileage list

--- vehicle/views.py
+++ vehicle/views.py
@@ -4,6 +4,9 @@ from rest_framework.generics import ListAPIView, UpdateAPIView, CreateAPIView, R
 from .models import Car, Moto, Mileage
 from .serializers import CarSerializer, MotoSerializer, MileageSerializer, MotoMileageSerializer, MotoCreateSerializer
 
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
+
 
 class CarViewSet(viewsets.ModelViewSet):
     queryset = Car.objects.all()
@@ -38,7 +41,14 @@ class MileageCreateAPIView(CreateAPIView):
     serializer_class = MileageSerializer
 
 
-class MotoMileageListAPIView(ListAPIView):
+class MileageListAPIView(ListAPIView):
+    queryset = Mileage.objects.all()
+    serializer_class = MileageSerializer
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ("car", "moto")
+    ordering_fields = ("year",)
+
+
+class MileageMotoListAPIView(ListAPIView):
     queryset = Mileage.objects.filter(moto__isnull=False)
     serializer_class = MotoMileageSerializer
-
